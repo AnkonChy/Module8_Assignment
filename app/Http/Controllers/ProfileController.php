0c342ccc6cba4ba40da2a69759823b980a93f6d6
Ankon-chowdhury <ankonchy1@gmail.com>
2023-11-18 18:19:22 +0600

    Assignment Uploaded

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ProfileController extends Controller
+{
+    function index( $id){
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookie = cookie('access_token', '123-XYZ', 1, '/', $_SERVER['SERVER_NAME'], false, true);
+
+    
+        return response()->json($data)->withCookie($cookie)->setStatusCode(200);
+    }
+}
